16ee4ae4e7321b5c0c13c14ead94322098305f8b
hailong <nicholas_ksd@hotmail.com>
2022-07-11 15:30:37 +0800

    update autok3s

--- autok3s-create-rancherha.sh
+++ autok3s-create-rancherha.sh
@@ -6,7 +6,7 @@ do
         curl https://raw.githubusercontent.com/cnrancher/autok3s/master/assets/rancher-setup/rancher-l7.sh | sh -
         exit 0
     else
-        echo "sss"
-        sleep 5
+        echo "Waiting for K3S service to start ..."
+        sleep 2
     fi
 done
