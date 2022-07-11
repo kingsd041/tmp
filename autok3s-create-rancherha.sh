2616bec957964c5a80002d9b9a8c4561d9865865
hailong <nicholas_ksd@hotmail.com>
2022-07-11 15:23:41 +0800

    update autok3s

--- autok3s-create-rancherha.sh
+++ autok3s-create-rancherha.sh
@@ -3,7 +3,7 @@
 while true
 do
     if [[ `systemctl is-active k3s` = "active" ]]; then
-        curl https://github.com/cnrancher/autok3s/blob/master/assets/rancher-setup/rancher-l7.sh | sh -
+        curl https://raw.githubusercontent.com/cnrancher/autok3s/master/assets/rancher-setup/rancher-l7.sh | sh -
         exit 0
     else
         echo "sss"
